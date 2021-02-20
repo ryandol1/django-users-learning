81091f13fbbca9747f25a077bf1291854edf3fa5
Ryan Dolinsky <ryandolinsky@Kierstins-MacBook-Pro.local>
2021-02-20 12:24:38 -0500

    added requirements

--- learning_users/Basic_App/views.py
+++ learning_users/Basic_App/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return render(request, 'Basic_App/index.html')
+    return render(request, str('Basic_App/index.html'))
 
 
 @login_required
